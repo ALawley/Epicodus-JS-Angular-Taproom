d7d04f4c268a7f5448dfe7288cd7a691405f37b8
Anna Lawley and Mary Warrington <marywarrington@gmail.com>
2016-03-23 08:46:12 -0700

    add keg constructor and kegs array

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,14 +1,22 @@
 import { Component } from 'angular2/core';
+import { Keg } from './keg.model.ts';
 
 @Component({
   selector: 'my-app',
   template: `
     <div class="container">
       <h1>Skeleton Angular2 App!</h1>
-    </div>  
+    </div>
   `
 })
 
 export class AppComponent {
-
+  public kegs: Keg[];
+  constructor() {
+    this.kegs = [
+      new Keg("Sculpin", "Ballast Point", "IPA", 6.00, 7.0),
+      new Keg("Farmhouse Ale", "Commons", "Belgian", 5.00, 5.3),
+      new Keg("White Rascal", "Avery", "White Ale", 5.00, 6.2)
+    ]
+  }
 }
---
+++ app/keg.model.ts
@@ -0,0 +1,6 @@
+export class Keg {
+  public pours: number = 124;
+  constructor(public name: string, public brand: string, public style: string, public price: number, public abv: number) {
+    
+  }
+}
